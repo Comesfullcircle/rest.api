26528252c67d13a56a0ea0edc5cba972a571dc9f
윤설이 <olerlmin1234@gmail.com>
2024-02-04 22:56:11 +0900

    Spring Boot Web - Spring Boot Web에서 응답 만드는 방법 Response Entity

---
+++ src/main/java/com/example/rest/api/controller/ResponseApiController.java
@@ -0,0 +1,34 @@
+package com.example.rest.api.controller;
+
+
+import com.example.rest.api.model.UserRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1")
+public class ResponseApiController {
+
+    //http://localhost:8080/api/v1
+    @GetMapping("")
+    public UserRequest user(){
+
+        var user = new UserRequest();
+        user.setUserName("홍길동");
+        user.setUserAge(10);
+        user.setEmail("hong@gmail.com");
+
+        log.info("user: {}", user);
+
+        var response = ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .header("x_custom", "hi")
+                .body(user);
+
+        return user;
+    }
+}
